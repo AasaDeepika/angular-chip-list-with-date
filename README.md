f03073a52b5427d8bc3d6ca423c4459e71d09bd1
AasaDeepika <ashadeepikaeee@gmail.com>
2023-05-03 18:20:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-chip-list-with-date
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/3aajwf-thwm9a)
---
+++ src/app/chips-input-example.ts
@@ -0,0 +1,68 @@
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Component} from '@angular/core';
+import {MatChipEditedEvent, MatChipInputEvent} from '@angular/material/chips';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
+export interface Fruit {
+  name: string;
+}
+
+/**
+ * @title Chips with input
+ */
+@Component({
+  selector: 'chips-input-example',
+  templateUrl: 'chips-input-example.html',
+  styleUrls: ['chips-input-example.css'],
+})
+export class ChipsInputExample {
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  fruits: Fruit[] = [{name: 'Lemon'}, {name: 'Lime'}, {name: 'Apple'}];
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruits.push({name: value});
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(fruit: Fruit): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
+
+  edit(fruit: Fruit, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove fruit if it no longer has a name
+    if (!value) {
+      this.remove(fruit);
+      return;
+    }
+
+    // Edit existing fruit
+    const index = this.fruits.indexOf(fruit);
+    if (index >= 0) {
+      this.fruits[index].name = value;
+    }
+  }
+
+  drop(event: CdkDragDrop<Fruit[]>) {
+    moveItemInArray(this.fruits, event.previousIndex, event.currentIndex);
+  }
+
+}
+
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
